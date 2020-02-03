c89505a070081ca3db617e7b483b05caf70a765d
Adenekan Wonderful <adenekanwonderful41@gmail.com>
2020-02-03 01:09:55 +0100

    (chore) added the main and jsnext:main to package.json

---
+++ readme.md
@@ -0,0 +1,46 @@
+![Drag Racing](https://codewonders.dev/icons/apple-icon-57x57.png)
+
+# [codewonders-helpers](https://github.com/adenekan41/helpers)
+
+> A Small Helpers Utility Functions For Javascript Development.
+
+## Install
+
+```sh
+$ npm install codewonders-helpers --save
+```
+
+## Usage
+
+Getting familiar with the package and you want to use some helper functions like
+`isEmpty`, `isJsonString` ,`pxToRem` helpers and e.t.c you can easily just
+export them the following ways.
+
+```js
+import { isEmpty } from 'codewonders-helpers'
+```
+
+All methods are exported as a flat namesapce so you can also call them like this
+
+```js
+import isEmpty from 'codewonders-helpers/bundle-es/is-empty'
+```
+
+or
+
+```js
+var isEmpty = require('codewonders-helpers')
+```
+
+and then we have the exported helpers and can already do this
+
+```js
+isEmpty({})
+// => true
+```
+
+> MIT © [codewonders.dev](https://codewonders.dev) &nbsp;&middot;&nbsp; GitHub
+> [@adenekan41 / codewonders](https://github.com/adenekan41)
+> &nbsp;&middot;&nbsp;
+
+<!-- {blockquote: style='display:none'} -->
--- rollup.config.js
+++ rollup.config.js
@@ -2,35 +2,34 @@ import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import multiInput from 'rollup-plugin-multi-input'
-import {
-    terser
-} from 'rollup-plugin-terser'
+import { terser } from 'rollup-plugin-terser'
 
 export default {
-    input: ['src/**/*.js', 'src/helpers/**/*.js'],
-    output: [{
-            dir: 'bundle-es',
-            format: 'es',
-            name: 'bundle',
-        },
-        {
-            dir: 'bundle-cjs',
-            format: 'cjs',
-            exports: 'named',
-            name: 'bundle',
-        },
-    ],
-    plugins: [
-        multiInput(),
-        babel({
-            exclude: 'node_modules/**',
-        }),
-        resolve({
-            jsnext: true,
-            main: true,
-            browser: true,
-        }),
-        commonjs(),
-        terser(),
-    ],
+  input: ['src/**/*.js', 'src/helpers/**/*.js'],
+  output: [
+    {
+      dir: 'bundle-es',
+      format: 'es',
+      name: 'bundle',
+    },
+    {
+      dir: 'bundle-cjs',
+      format: 'cjs',
+      exports: 'named',
+      name: 'bundle',
+    },
+  ],
+  plugins: [
+    multiInput(),
+    babel({
+      exclude: 'node_modules/**',
+    }),
+    resolve({
+      jsnext: true,
+      main: true,
+      browser: true,
+    }),
+    commonjs(),
+    terser(),
+  ],
 }
